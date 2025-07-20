67f448ece520307a6d24e771f5d14b145a265244
AbdullahKhan-786 <abdullahawaiskhan2511@gmail.com>
2025-07-20 20:29:05 +0500

    Add files via upload

---
+++ USER ALWAYS LOSSING GAME.py
@@ -0,0 +1,7 @@
+x=101
+while True:
+    y=int(input('PLEASE ENTER A NUMBER SAME AS COMPUTER VALUE UNDER 100'))    
+    if y==x:
+      print('OMG! YOU WON')
+    elif y!=x:
+      print('OH NO! YOU HAVE LOST AGINST A COMPUTER')
